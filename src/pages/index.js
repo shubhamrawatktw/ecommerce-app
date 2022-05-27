6362dcfb218748b57bf6c60ce095e5a532edbb87
shubham <shubhamrawatktw@gmail.com>
2022-05-27 11:28:19 +0530

    starter setup

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import Private from "./PrivateRoute";
+
+export { Home, About, Products, SingleProduct, Cart, Error, Checkout, Private };
