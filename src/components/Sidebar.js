67ea310b72b28317af9bdbd57d4675c97aae11d1
shubham <shubhamrawatktw@gmail.com>
2022-05-27 12:16:58 +0530

    open & close sidebar funct

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,23 +9,25 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
-  const isOpen = false
+  const {openSidebar,closeSidebar,isSidebarOpen} = useProductsContext()
+ 
+  
   return <SidebarContainer>
-    <aside className={`${isOpen ? "sidebar show-sidebar" :"sidebar"}`}>
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" :"sidebar"}`}>
       <div className="sidebar-header">
         <img src={logo} className="logo" alt="comfy" />
-        <button className='close-btn' type='button'>
+        <button className='close-btn' type='button' onClick={closeSidebar}>
           <FaTimes/>
         </button>
       </div>
       <ul className="links">
         {links.map(({id,text,url})=>{
           return <li key={id}>
-           <Link to={url}>{text}</Link>
+           <Link to={url}  onClick={closeSidebar}>{text}</Link>
           </li>
         })}
         <li>
-          <Link to="/checkout">checkout</Link>
+          <Link to="/checkout"  onClick={closeSidebar}>checkout</Link>
         </li>
       </ul>
       <CartButtons/>
