ac28c8339542b9054e02a865e02030436be48127
shubham <shubhamrawatktw@gmail.com>
2022-05-29 13:57:11 +0530

    filter context & products page - list & grid view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,41 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from '../assets/logo.svg'
-// import logo from "../assets/logo2.jpg"
-import { FaBars } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { links } from '../utils/constants'
-import CartButtons from './CartButtons'
-import { useProductsContext } from '../context/products_context'
-import { useUserContext } from '../context/user_context'
+import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo2.jpg";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { links } from "../utils/constants";
+import CartButtons from "./CartButtons";
+import { useProductsContext } from "../context/products_context";
+import { useUserContext } from "../context/user_context";
 
 const Nav = () => {
-  const {openSidebar} = useProductsContext()
-  return <NavContainer>
-     <div className="nav-center">
-       <div className="nav-header">
-         <Link to='/'>
-          <img src={logo} alt="logo" />
-         </Link>
-         <button type='button' className='nav-toggle' onClick={openSidebar}>
-           <FaBars/>
-         </button>
-       </div>
-       <ul className="nav-links">
-         {links.map((link)=>{
-           const {id,text,url} = link
-           return <li key={id}>
-             <Link to={url}>
-               {text}
-             </Link>
-           </li>
-         })}
-       </ul>
-       <CartButtons/>
-     </div>
-  </NavContainer>
-}
+  const { openSidebar } = useProductsContext();
+  return (
+    <NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <button type="button" className="nav-toggle" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <CartButtons />
+      </div>
+    </NavContainer>
+  );
+};
 
 const NavContainer = styled.nav`
   height: 5rem;
@@ -53,7 +54,7 @@ const NavContainer = styled.nav`
     justify-content: space-between;
     img {
       width: 175px;
-       height:50px;
+      height: 50px;
       margin-left: -15px;
     }
   }
@@ -102,6 +103,6 @@ const NavContainer = styled.nav`
       display: grid;
     }
   }
-`
+`;
 
-export default Nav
+export default Nav;
