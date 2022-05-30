2c10213a7e43fb0d932d3555f2d396e15e41d2da
shubham <shubhamrawatktw@gmail.com>
2022-05-30 22:55:47 +0530

    filter functionality done

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -17,16 +17,16 @@ const initialState = {
   all_products: [],
   grid_view: true,
   sort: "price-lowest",
-  filters:{
-    text:"",
-    company:"all",
-    category:"all",
-    color:"all",
-    min_price:0,
-    max_price:0,
-    price:0,
-    shipping:false
-  }
+  filters: {
+    text: "",
+    company: "all",
+    category: "all",
+    color: "all",
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  },
 };
 
 const FilterContext = React.createContext();
@@ -38,12 +38,11 @@ export const FilterProvider = ({ children }) => {
   useEffect(() => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
   }, [products]);
-  
-useEffect(()=>{
-  dispatch({type:FILTER_PRODUCTS})
- dispatch({type:SORT_PRODUCTS})
-},[products,state.sort,state.filters])
 
+  useEffect(() => {
+    dispatch({ type: FILTER_PRODUCTS });
+    dispatch({ type: SORT_PRODUCTS });
+  }, [products, state.sort, state.filters]);
 
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW });
@@ -55,30 +54,30 @@ useEffect(()=>{
   const updateSort = (e) => {
     //  const name = e.target.name
     const value = e.target.value;
-    dispatch({type:UPDATE_SORT,payload:value})
+    dispatch({ type: UPDATE_SORT, payload: value });
   };
 
-  const updateFilters = (e)=>{
-   let name = e.target.name
-   let value = e.target.value
-   if (name === "category") {
-     value = e.target.textContent
-   }
-   if (name === "color") {
-     value = e.target.dataset.color
-   }
-   if (name === "price") {
-     value = Number(value)
-   }
-   if (name === "shipping") {
-     value = e.target.checked
-   }
-   dispatch({type:UPDATE_FILTERS,payload:{name,value}})
-  }
+  const updateFilters = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    if (name === "category") {
+      value = e.target.textContent;
+    }
+    if (name === "color") {
+      value = e.target.dataset.color;
+    }
+    if (name === "price") {
+      value = Number(value);
+    }
+    if (name === "shipping") {
+      value = e.target.checked;
+    }
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
+  };
 
-  const clearFilters = ()=>{
-    dispatch({type:CLEAR_FILTERS})
-  }
+  const clearFilters = () => {
+    dispatch({ type: CLEAR_FILTERS });
+  };
 
   return (
     <FilterContext.Provider
@@ -88,7 +87,7 @@ useEffect(()=>{
         setListView,
         updateSort,
         updateFilters,
-        clearFilters
+        clearFilters,
       }}
     >
       {children}
