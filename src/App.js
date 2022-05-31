846ccb620520588219b45aff0fd58ecb0b86a4e4
shubham <shubhamrawatktw@gmail.com>
2022-05-31 21:02:08 +0530

    fixes bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
 import {
@@ -14,33 +14,24 @@ import {
   AuthWrapper,
 } from "./pages";
 
+
 function App() {
   return (
     <AuthWrapper>
       <Router>
         <Navbar />
         <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/cart">
-            <Cart />
-          </Route>
-          <Route exact path="/products">
-            <Products />
-          </Route>
-          <Route exact path="/products/:id" children={<SingleProduct />} />
-          <Private exact path="/checkout">
-            <Checkout />
-          </Private>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="cart" element={<Cart/>} />
+        <Route path="products" element={<Products/>} />
+        <Route path="products/:id" element={<SingleProduct/>} />
+        <Route path="checkout" element={<Private>
+          <Checkout/>
+        </Private>} />
+        <Route path="*" element={<Error/>} />
+        </Routes>
         <Footer />
       </Router>
     </AuthWrapper>
