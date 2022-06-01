25eb75bfa0097178a63b157e5f6f9fbbdaa9e9e0
shubham <shubhamrawatktw@gmail.com>
2022-06-01 15:44:35 +0530

    finished

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import logo from "../assets/logo.svg";
+
+import logo from "../assets/logo2.jpg"
 import { Link } from "react-router-dom";
 import { useProductsContext } from "../context/products_context";
 import { FaTimes } from "react-icons/fa";
